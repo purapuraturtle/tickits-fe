8581c010a56b260dc4eb8567467aa1cc364db997
afif-buchori <4fifbuchori@gmail.com>
2023-04-30 13:10:42 +0800

    admin page 60%

--- src/pages/ticket-result.jsx
+++ src/pages/ticket-result.jsx
@@ -8,20 +8,25 @@ function TicketResult() {
       <div className="w-full flex justify-center items-center min-h-[76vh] bg-slate-400/20 py-16">
         <section className="w-full max-w-[1440px] flex flex-col justify-center items-center bg-white rounded-md p-5 md:px-10">
           <h1 className="text-2xl font-bold">Proof of Payment</h1>
-          <table className="w-full gap-5 rounded-xl border overflow-hidden text-center table-ticket">
-            <tr className="bg-primary">
+          <div className="w-full gap-5 rounded-xl overflow-hidden text-center table-ticket">
+            <tr className="bg-primary w-full">
               <td colSpan={2} className="font-bold text-4xl">
                 TICKITZ
               </td>
               <td>Admit One</td>
-              <td className="font-bold text-4xl w-[400px]">TICKITZ</td>
+              <td className="font-bold text-4xl w-[400px] border-l border-black border-dashed">
+                TICKITZ
+              </td>
             </tr>
             <tr>
               <td colSpan={3} className="text-start">
                 <p className="text-gray-600">Movie</p>
                 <p className="text-lg font-bold">Spider-Man: Homecoming</p>
               </td>
-              <td rowSpan={3}>
+              <td
+                rowSpan={3}
+                className="border-l border-black border-dashed ticket-cut"
+              >
                 <p className="text-8xl">CODE</p>
               </td>
             </tr>
@@ -53,7 +58,7 @@ function TicketResult() {
                 <p className="text-lg font-bold">$30.00</p>
               </td>
             </tr>
-          </table>
+          </div>
         </section>
       </div>
       <Footer />
