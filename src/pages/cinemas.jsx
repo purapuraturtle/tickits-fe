8304444b49832c9fd347d69f50535cbead67a619
zikriaulia28 <zikriaulia98@gmail.com>
2023-05-03 11:16:34 +0700

    add cinemas

---
+++ src/pages/cinemas.jsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import Layout from "@/components/Layout";
+
+function Cinemas() {
+  return (
+    <Layout>
+      <Header />
+
+      <Footer />
+    </Layout>
+  );
+}
+
+export default Cinemas;
